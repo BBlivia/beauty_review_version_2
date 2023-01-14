99d16fdab5810956cb9f3c60d748afbf6a902efb
Olivia Bloway <blowayol@msu.edu>
2023-01-14 18:56:48 -0500

    routes now protected

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,13 +1,13 @@
 
 const express = require("express")
 const router = express.Router()
-//const { } = require('../middleware/authMiddleware')
+const {protect} = require("../middleware/authMiddleware")
 const {getAllReviews, createReview, updateReview, deleteReview, getMyReviews} = require("../controllers/reviewController")
 
 
-router.route("/").get( getAllReviews).post( createReview)
-router.route("/:id").put(updateReview).delete(deleteReview) 
-router.route('/myreviews').get( getMyReviews)
+router.route("/").get(protect, getAllReviews).post(protect, createReview)
+router.route("/:id").put(protect,updateReview).delete(protect,deleteReview) 
+router.route('/myreviews').get(protect, getMyReviews)
 
 
 
