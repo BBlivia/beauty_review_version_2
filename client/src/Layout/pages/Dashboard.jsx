8384bf8e529002043fd1c3c7ace9684012bee5b8
Olivia Bloway <blowayol@msu.edu>
2023-01-26 14:45:18 -0500

    routes protected

--- client/src/Layout/pages/Dashboard.jsx
+++ client/src/Layout/pages/Dashboard.jsx
@@ -1,8 +1,19 @@
 import Review from '../components/Review'
 
+import {useNavigate} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import { useEffect } from 'react'
 
 
 export default function Dashboard(){
+    const navigate = useNavigate()
+const {user} = useSelector((state) => state.auth)
+
+useEffect(() =>{
+    if(!user){
+        navigate('/login')
+    }
+}, [user, navigate])
     return(
         <div>
             <Review />
